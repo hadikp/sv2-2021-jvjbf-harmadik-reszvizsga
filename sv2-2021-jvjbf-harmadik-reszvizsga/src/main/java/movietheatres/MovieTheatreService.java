ecba2ec8536393fd1075853152c6380f79ed201c
hadikp <hp20001025@gmail.com>
2022-01-31 11:43:13 +0100

    movietheatres

---
+++ sv2-2021-jvjbf-harmadik-reszvizsga/src/main/java/movietheatres/MovieTheatreService.java
@@ -0,0 +1,82 @@
+package movietheatres;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalTime;
+import java.util.*;
+
+public class MovieTheatreService {
+
+    private Movie movie = new Movie("",LocalTime.now());
+    private Map<String, List<Movie>> shows = new HashMap<>();
+
+    public Map<String, List<Movie>> getShows() {
+        return shows;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void readFromFile(Path path) {
+
+        try {
+            List<String> movies = Files.readAllLines(path);
+            for(int i = 0; i < movies.size(); i++){
+                String cinema = movies.get(i).split("-")[0];
+                String moviedatas = movies.get(i).split("-")[1];
+
+                String title = moviedatas.split(";")[0];
+                movie.setTitle(title);
+                String time = moviedatas.split(";")[1];
+                int timeHour = Integer.parseInt(time.split(":")[0]);
+                int timeMinutes = Integer.parseInt(time.split(":")[1]);
+                movie.setStartTime(LocalTime.of(timeHour,timeMinutes));
+
+                if(shows.containsKey(cinema)){
+                    List<Movie> cinemaname = shows.get(cinema);
+                    cinemaname.add(movie);
+                    cinemaname.stream().sorted(Comparator.comparing(Movie::getStartTime));
+                    shows.put(cinema,cinemaname);
+
+                }
+                if(!shows.containsKey(cinema)){
+                    List<Movie> cinemaname = new ArrayList<>();
+                    cinemaname.add(movie);
+                    cinemaname.stream().sorted(Comparator.comparing(Movie::getStartTime));
+                    shows.put(cinema,cinemaname);
+                }
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public List<String> findMovie(String title){
+        List<String> movies = new ArrayList<>();
+        for(Map.Entry<String,List<Movie>> entries: shows.entrySet()){
+            if((entries.getValue().stream().map(p->p.getTitle()).filter(f -> f.equals(title)))) {
+                movies.add(entries.getKey());
+            }
+        }
+        return movies;
+    }
+
+    public LocalTime findLatestShow(String title){
+        List<Movie> moviesLatest = new ArrayList<>();
+        for(Map.Entry<String,List<Movie>> entries: shows.entrySet()){
+            if((entries.getValue().contains(title))){
+               // moviesLatest.add(entries.getValue());
+            } else {
+                findLatestShowWithWrongName();
+            }
+        }
+        return null;
+    }
+
+    public void findLatestShowWithWrongName(){
+        throw  new IllegalArgumentException("Can't find title!");
+    }
+}
