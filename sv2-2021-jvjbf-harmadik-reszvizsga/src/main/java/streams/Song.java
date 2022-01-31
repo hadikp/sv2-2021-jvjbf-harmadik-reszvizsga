62f594e793b081959b0141692d2fcfb6687b3fab
hadikp <hp20001025@gmail.com>
2022-01-31 09:45:04 +0100

    dalok

--- sv2-2021-jvjbf-harmadik-reszvizsga/src/main/java/streams/Song.java
+++ sv2-2021-jvjbf-harmadik-reszvizsga/src/main/java/streams/Song.java
@@ -1,5 +1,35 @@
 package streams;
 
-public class Song {
+import java.time.LocalDate;
+import java.util.List;
 
+public class Song {
+    private String title;
+    private int length; //s-ban
+    private List<String> performers;
+    private LocalDate release;
+
+
+    public Song(String title, int length, List<String> performers, LocalDate release) {
+        this.title = title;
+        this.length = length;
+        this.performers = performers;
+        this.release = release;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public List<String> getPerformers() {
+        return performers;
+    }
+
+    public LocalDate getRelease() {
+        return release;
+    }
 }
---
+++ sv2-2021-jvjbf-harmadik-reszvizsga/src/main/java/streams/SongService.java
@@ -0,0 +1,37 @@
+package streams;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class SongService {
+
+    private List<Song> songs = new ArrayList<>();
+
+    public void addSong (Song song) {
+        songs.add(song);
+    }
+
+    public Optional<Song> shortestSong() {
+        return songs.stream().min(Comparator.comparing(Song::getLength));
+    }
+
+    public List<Song> findSongByTitle(String title) {
+        return songs.stream().filter(s -> s.getTitle().equals(title)).collect(Collectors.toList());
+    }
+
+    public boolean isPerformerInSong(Song song, String performer) {
+        return songs.stream().anyMatch(s -> s.getPerformers().contains(performer) && s.getPerformers().contains(song));
+    }
+
+    public List<String> titlesBeforeDate (LocalDate date) {
+        return songs.stream().filter(s -> s.getRelease().isBefore(date)).map(p -> p.getTitle()).collect(Collectors.toList());
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+}
